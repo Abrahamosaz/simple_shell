de1a47621420ff7588c02fc9fd85e54b19014e3f
Orumgbe <orumgbeugbekile@gmail.com>
2022-08-05 05:29:43 -0700

    adding comment

--- _strtok.c
+++ _strtok.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * _strtok - tokenizes string
+ * @str: string to be tokenized
+ * @delim: breakpoint character
+ * Return: string token
+ */
 char *_strtok(char *str, char delim)
 {
-	static char *buffer = NULL;
+	static char *buffer;
 
 	if (buffer == NULL)
 		buffer = str;
-	if (!*buffer)
+	if (*buffer == '\0')
 		return (NULL);
 
 	char *token = buffer;
+
 	while (*buffer && *buffer != delim)
 	{
-		buffer++;
+		if (*buffer != delim)
+			buffer++;
+		else
+			break;
 	}
 	if (*buffer)
-	{	
+	{
 		*buffer = '\0';
 		buffer++;
 	}
 	return (token);
 }
+/**
+ * main - Code entrypoint
+ * Return: 0
+ */
 int main(void)
 {
 	char str[80] = "This is a test of strtok";
